d5e88acdad1598731a42eea521cd8b6553d7829e
Huzaifa Khan <huzaifagmer@gmail.com>
2025-07-21 02:33:34 +0500

    SEO Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import pages from 'vite-plugin-pages'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    pages(), 
+    sitemap({
+      hostname: 'https://sunamemusic.com',
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
@@ -23,4 +31,4 @@ export default defineConfig({
       usePolling: true,
     },
   },
-});
+})
